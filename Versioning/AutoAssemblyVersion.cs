11050a904b56a108b5074c438361e72a20e51f3b
Trevor Vass <trevor@trevorvass.com>
2015-05-26 11:34:43 -0700

    Added an XML comment for ActiveDirectoryRoleProvider and fixed the wording of an exception to be more accurate. Added SimpleMappingRoleProvider class. Allows for adding users to roles stored in a configuration item. Updated the solution version to 1.1.0.71.

--- Versioning/AutoAssemblyVersion.cs
+++ Versioning/AutoAssemblyVersion.cs
@@ -7,5 +7,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.65")]
-[assembly: AssemblyFileVersion("1.1.0.65")]
+[assembly: AssemblyVersion("1.1.0.71")]
+[assembly: AssemblyFileVersion("1.1.0.71")]
